09668eba74a5aef06acfcb42b445dd526542b182
Chris Waters <bieoe.ffe@gmail.com>
2021-01-22 19:20:31 -0600

    Minor restructuring to reduce changes to concurrencyCount

--- src/queue.js
+++ src/queue.js
@@ -4,25 +4,23 @@ function sleep (ms) {
 
 async function concurrencyControl (Queue) {
   while (true) {
-    if (Queue.concurrencyCount < Queue.maxConcurrency) {
-      Queue.concurrencyCount++
-      return
-    }
+    if (Queue.concurrencyCount < Queue.maxConcurrency) return
     await sleep(100)
   }
 }
 
 function runNext (Queue) {
   const next = Queue.items.shift()
-  if (next === undefined) Queue.concurrencyCount--
-  else Promise.resolve(next.func(...next.params)).then(() => Queue.concurrencyCount--)
+  if (next === undefined) return
+  Queue.concurrencyCount++
+  Promise.resolve(next.func(...next.params)).then(() => Queue.concurrencyCount--)
 }
 
 async function run (Queue) {
   while (Queue._executing && Queue.items.length) {
     await concurrencyControl(Queue)
-    if (Queue._executing === false) Queue.concurrencyCount--
-    else runNext(Queue)
+    if (Queue._executing === false) return
+    runNext(Queue)
   }
   while (Queue.concurrencyCount > 0) {
     await sleep(100)
